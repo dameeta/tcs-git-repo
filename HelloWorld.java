c69eebb508fab526674e6c719820098bc97da45a
Dameeta <85731431+dameeta@users.noreply.github.com>
2024-04-15 19:02:35 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello-World!!");
+    }
+
+    public void ADD(int a,int b,int c,int d,int e, int f,int j,int k)
+    {
+        System,out.println("some data... in ADD method")
+    }
+}
